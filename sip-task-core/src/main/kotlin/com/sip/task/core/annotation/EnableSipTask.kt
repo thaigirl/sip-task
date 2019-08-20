f7834581daed6796a3ceeacb58bf3928a3da2839
tianhu <18910751785@163.com>
2019-08-20 15:36:36 +0800

    remove @Inherited

--- sip-task-core/src/main/kotlin/com/sip/task/core/annotation/EnableSipTask.kt
+++ sip-task-core/src/main/kotlin/com/sip/task/core/annotation/EnableSipTask.kt
@@ -5,13 +5,11 @@ import com.sip.task.core.configuration.FeignRegistrar
 import com.sip.task.core.configuration.ScheduleSpringConfiguration
 import org.springframework.context.annotation.Import
 import java.lang.annotation.Documented
-import java.lang.annotation.Inherited
 import java.lang.annotation.Retention
 import java.lang.annotation.RetentionPolicy
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FILE)
 @Documented
-@Inherited
 @Import(BeanRegisterConfiguration::class, FeignRegistrar::class, ScheduleSpringConfiguration::class)
 annotation class EnableSipTask
