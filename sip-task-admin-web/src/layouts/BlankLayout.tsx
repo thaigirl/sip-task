1e3b5e6702d293e1f533d355c569492d205bc684
tianhu <18910751785@163.com>
2019-08-20 14:29:03 +0800

    init web

---
+++ sip-task-admin-web/src/layouts/BlankLayout.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import CopyBlock from '@/components/CopyBlock';
+import CopyBlock from '@/components/CopyBlock';
+
+const Layout: React.FC = ({ children }) => (
+  <>
+    <>
+      <div>{children}</div>
+      <CopyBlock id={Date.now()} />
+    </>
+    <CopyBlock id={Date.now()} />
+  </>
+);
+
+export default Layout;
