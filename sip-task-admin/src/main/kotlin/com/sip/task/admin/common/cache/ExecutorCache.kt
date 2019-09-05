e1cad3f9a27db8fc602c456931c39d16eabf705d
tianhu <18910751785@163.com>
2019-09-05 17:16:37 +0800

    `ExecutorCache` add remove method

--- sip-task-admin/src/main/kotlin/com/sip/task/admin/common/cache/ExecutorCache.kt
+++ sip-task-admin/src/main/kotlin/com/sip/task/admin/common/cache/ExecutorCache.kt
@@ -11,13 +11,22 @@ object ExecutorCache {
 
     private var EXECUTOR: MutableMap<Long, QrtzTriggerExecutor> = mutableMapOf()
 
-
     fun set(id: Long, executor: QrtzTriggerExecutor) {
         EXECUTOR[id] = executor
     }
 
+    fun init(executors: List<QrtzTriggerExecutor>) {
+        EXECUTOR = executors.associateBy({ it.id!! }, { it }).toMutableMap()
+    }
+
     fun get(id: Long): QrtzTriggerExecutor {
         return EXECUTOR[id] ?: throw CustomException("not found cache with id:$id")
+    }
+
+    fun remove(id: Long) {
+        if (EXECUTOR.containsKey(id)) {
+            EXECUTOR.remove(id)
+        }
     }
 
 }
