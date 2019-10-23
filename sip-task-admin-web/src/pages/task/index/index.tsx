3f58df65275d6db57afc9c223da00c5dd931ffef
lucoxlee <lucoxlee@gmail.com>
2019-10-23 18:17:03 +0800

    fix: bug

--- sip-task-admin-web/src/pages/task/index/index.tsx
+++ sip-task-admin-web/src/pages/task/index/index.tsx
@@ -1,33 +1,12 @@
-import {Card, Col, Form, Modal, Row, Select} from 'antd';
+import {Card, Col, Form, Row} from 'antd';
 import React, {Component} from 'react';
-import {
-  G2,
-  Chart,
-  Geom,
-  Axis,
-  Tooltip,
-  Coord,
-  Label,
-  Legend,
-  View,
-  Guide,
-  Shape,
-  Facet,
-  Util
-} from "bizcharts";
+import {Axis, Chart, Coord, Geom, Guide, Label, Legend, Tooltip,} from "bizcharts";
 import DataSet from "@antv/data-set";
 
 import {Dispatch} from 'redux';
 import {FormComponentProps} from 'antd/es/form';
 import {connect} from 'dva';
-// @ts-ignore
 import {StateType} from './model';
-import {indexDto} from './data.d';
-
-const FormItem = Form.Item;
-const {Option} = Select;
-const {confirm} = Modal;
-
 
 interface TableListProps extends FormComponentProps {
   dispatch: Dispatch<any>;
@@ -52,7 +31,7 @@ interface State {
       };
     };
   }) => ({
-    job:index,
+    job: index,
     loading: loading.models.rule,
   }),
 )
@@ -75,10 +54,10 @@ class TableList extends Component<TableListProps, State> {
 
   render() {
     const {
-      job: {triggerInfo,lineChartInfo}
+      job: {triggerInfo, lineChartInfo}
     } = this.props;
-    const { DataView } = DataSet;
-    const { Html } = Guide;
+    const {DataView} = DataSet;
+    const {Html} = Guide;
     const data = [
       {
         item: "待执行",
@@ -110,9 +89,7 @@ class TableList extends Component<TableListProps, State> {
     });
     const cols = {
       percent: {
-        formatter: (val:number) => {
-          return (val * 100).toFixed(2) + "%";
-        }
+        formatter: (val: number) => `${(val * 100).toFixed(2)}%`
       }
     };
 
@@ -129,40 +106,131 @@ class TableList extends Component<TableListProps, State> {
 
 
     return (
-      <Row gutter={{md: 8, lg: 24, xl: 48}}>
-        <Col span={7}>
-          <Card title="执行器信息" extra={<a href="#">More</a>} style={{width: '100%'}}>
-            <p>Card content</p>
-            <p>Card content</p>
-          </Card>
-        </Col>
-        <Col span={8}>
-          <Card title="调度任务数:20" extra={<a href="#">More</a>} style={{width: '100%'}}>
-            <p>已启用:2</p>
-            <p>未启用:5</p>
-          </Card>
-        </Col>
-        <Col span={9}>
-          <Card title={`调度次数:${triggerInfo.recordInfo.totalCount}`} extra={<a href="#">More</a>} style={{ width: '100%' }}>
-            {/*<p>运行中:{triggerInfo.recordInfo.runingCount}</p>*/}
-            {/*<p>已成功:{triggerInfo.recordInfo.successCount}</p>*/}
-            {/*<p>已失败:{triggerInfo.recordInfo.failCount}</p>*/}
-            {/*<p>已超时:{triggerInfo.recordInfo.timeOutCount}</p>*/}
-            {/*<p>待执行:{triggerInfo.recordInfo.waitExecCount}</p>*/}
+      <div>
+        <Row gutter={{md: 8, lg: 24, xl: 48}}>
+          <Col span={5}>
+            <Card title="执行器信息" extra={<a href="#">More</a>} style={{width: '100%'}}>
+              <p>Card content</p>
+              <p>Card content</p>
+            </Card>
+          </Col>
+          <Col span={5}>
+            <Card title="调度任务数:20" extra={<a href="#">More</a>} style={{width: '100%'}}>
+              <p>已启用:2</p>
+              <p>未启用:5</p>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={24}>
+            <div style={{height: 24}}></div>
+          </Col>
+        </Row>
+        <Row gutter={{md: 8, lg: 24, xl: 48}}>
+          <Col span={12}>
+            <Card title={`调度次数:${triggerInfo.recordInfo.totalCount}`} extra={<a href="#">More</a>}
+                  style={{width: '100%'}}>
+              <div>
+                <Chart
+                  height={400}
+                  data={dv}
+                  scale={cols}
+                  padding={[20, 200, 20, 20]}
+                  forceFit
+                >
+                  <Coord type="theta" radius={0.75} innerRadius={0.6}/>
+                  <Axis name="percent"/>
+                  <Legend
+                    position="right"
+                    offsetY={-250}
+                    offsetX={10}
+                  />
+                  <Tooltip
+                    showTitle={false}
+                    itemTpl="<li><span style=&quot;background-color:{color};&quot; class=&quot;g2-tooltip-marker&quot;></span>{name}: {value}</li>"
+                  />
+                  <Guide>
+                    <Html
+                      position={["50%", "50%"]}
+                      html={`<div style="color:#8c8c8c;font-size:1.16em;text-align: center;width: 10em;">调度次数<br><span style="color:#262626;font-size:2.5em"></span>${triggerInfo.recordInfo.totalCount}次</div>`}
+                      alignX="middle"
+                      alignY="middle"
+                    />
+                  </Guide>
+                  <Geom
+                    type="intervalStack"
+                    position="percent"
+                    color="item"
+                    tooltip={[
+                      "item*percent",
+                      (item, percent) => {
+                        percent = `${percent * 100}%`;
+                        return {
+                          name: item,
+                          value: percent
+                        };
+                      }
+                    ]}
+                    style={{
+                      lineWidth: 1,
+                      stroke: "#fff"
+                    }}
+                  >
+                    <Label
+                      content="percent"
+                      formatter={(val, item) => `${item.point.item}: ${val}`}
+                    />
+                  </Geom>
+                </Chart>
+              </div>
+            </Card>
+          </Col>
+          <Col span={12}>
+            <Card title={`调度次数:${triggerInfo.recordInfo.totalCount}`} extra={<a href="#">More</a>}
+                  style={{width: '100%'}}>
+              <div>
+                <Chart data={triggerData} scale={triggerCols} forceFit height={400}>
+                  <Axis
+                    name="month"
+                  />
+                  <Axis
+                    name="acc"
+                  />
+                  <Tooltip/>
+                  <Geom
+                    type="line"
+                    position="month*acc"
+                    size={1}
+                    color="l (270) 0:rgba(255, 146, 255, 1) .5:rgba(100, 268, 255, 1) 1:rgba(215, 0, 255, 1)"
+                    shape="smooth"
+                    style={{
+                      shadowColor: "l (270) 0:rgba(21, 146, 255, 0)",
+                      shadowBlur: 60,
+                      shadowOffsetY: 6
+                    }}
+                  />
+                </Chart>
+              </div>
+            </Card>
+          </Col>
+        </Row>
+
+        <Row gutter={{md: 8, lg: 24, xl: 48}}>
+          <Col span={12}>
             <div>
               <Chart
-                height={window.innerHeight}
+                height={400}
                 data={dv}
                 scale={cols}
-                // padding={[80, 100, 80, 80]}
+                padding={[20, 200, 20, 20]}
                 forceFit
               >
-                <Coord type={"theta"} radius={0.75} innerRadius={0.6} />
-                <Axis name="percent" />
+                <Coord type="theta" radius={0.75} innerRadius={0.6}/>
+                <Axis name="percent"/>
                 <Legend
                   position="right"
-                  offsetY={-window.innerHeight / 2 + 120}
-                  offsetX={-100}
+                  offsetY={-250}
+                  offsetX={10}
                 />
                 <Tooltip
                   showTitle={false}
@@ -171,7 +239,7 @@ class TableList extends Component<TableListProps, State> {
                 <Guide>
                   <Html
                     position={["50%", "50%"]}
-                    html={`<div style=&quot;color:#8c8c8c;font-size:1.16em;text-align: center;width: 10em;&quot;>调度次数<br><span style=&quot;color:#262626;font-size:2.5em&quot;></span>${triggerInfo.recordInfo.totalCount}次</div>`}
+                    html={`<div style="color:#8c8c8c;font-size:1.16em;text-align: center;width: 10em;">调度次数<br><span style="color:#262626;font-size:2.5em"></span>${triggerInfo.recordInfo.totalCount}次</div>`}
                     alignX="middle"
                     alignY="middle"
                   />
@@ -183,7 +251,7 @@ class TableList extends Component<TableListProps, State> {
                   tooltip={[
                     "item*percent",
                     (item, percent) => {
-                      percent = percent * 100 + "%";
+                      percent = `${percent * 100}%`;
                       return {
                         name: item,
                         value: percent
@@ -197,51 +265,39 @@ class TableList extends Component<TableListProps, State> {
                 >
                   <Label
                     content="percent"
-                    formatter={(val, item) => {
-                      return item.point.item + ": " + val;
-                    }}
+                    formatter={(val, item) => `${item.point.item}: ${val}`}
                   />
                 </Geom>
               </Chart>
             </div>
-          </Card>
-        </Col>
-        <Col span={7}>
-          <div>
-            <Chart width={400} height={400} data={triggerData} scale={triggerCols} forceFit>
-              <Axis
-                name="month"
-                title={null}
-                tickLine={null}
-                line={{
-                  stroke: "#E6E6E6"
-                }}
-              />
-              <Axis
-                name="acc"
-                line={false}
-                tickLine={null}
-                grid={null}
-                title={null}
-              />
-              <Tooltip />
-              <Geom
-                type="line"
-                position="month*acc"
-                size={1}
-                color="l (270) 0:rgba(255, 146, 255, 1) .5:rgba(100, 268, 255, 1) 1:rgba(215, 0, 255, 1)"
-                shape="smooth"
-                style={{
-                  shadowColor: "l (270) 0:rgba(21, 146, 255, 0)",
-                  shadowBlur: 60,
-                  shadowOffsetY: 6
-                }}
-              />
-            </Chart>
-          </div>
-        </Col>
-      </Row>
-
+          </Col>
+          <Col span={12}>
+            <div>
+              <Chart data={triggerData} scale={triggerCols} forceFit height={400}>
+                <Axis
+                  name="month"
+                />
+                <Axis
+                  name="acc"
+                />
+                <Tooltip/>
+                <Geom
+                  type="line"
+                  position="month*acc"
+                  size={1}
+                  color="l (270) 0:rgba(255, 146, 255, 1) .5:rgba(100, 268, 255, 1) 1:rgba(215, 0, 255, 1)"
+                  shape="smooth"
+                  style={{
+                    shadowColor: "l (270) 0:rgba(21, 146, 255, 0)",
+                    shadowBlur: 60,
+                    shadowOffsetY: 6
+                  }}
+                />
+              </Chart>
+            </div>
+          </Col>
+        </Row>
+      </div>
     );
   }
 }
--- sip-task-admin/src/main/kotlin/com/sip/task/admin/AdminApplication.kt
+++ sip-task-admin/src/main/kotlin/com/sip/task/admin/AdminApplication.kt
@@ -3,7 +3,6 @@ package com.sip.task.admin
 import com.basicfu.sip.core.annotation.EnableSipCore
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.SpringCloudApplication
-import org.springframework.cloud.openfeign.EnableFeignClients
 import tk.mybatis.spring.annotation.MapperScan
 
 @MapperScan(basePackages = ["com.sip.task.admin.mapper"])
