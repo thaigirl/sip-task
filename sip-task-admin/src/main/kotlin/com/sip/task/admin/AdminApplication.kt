2952a4eeb995d6c1d5151cc30a03664ef008dd95
lucoxlee <lucoxlee@gmail.com>
2019-10-28 15:10:34 +0800

    Merge branch 'master' of /Users/admin/IdeaProjects/sip-task with conflicts.

--- sip-task-admin/src/main/kotlin/com/sip/task/admin/AdminApplication.kt
+++ sip-task-admin/src/main/kotlin/com/sip/task/admin/AdminApplication.kt
@@ -1,7 +1,6 @@
 package com.sip.task.admin
 
 import com.basicfu.sip.core.annotation.EnableSipCore
-import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.SpringCloudApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
@@ -10,6 +9,7 @@ import tk.mybatis.spring.annotation.MapperScan
 @MapperScan(basePackages = ["com.sip.task.admin.mapper"])
 @EnableSipCore
 @SpringCloudApplication
+@EnableFeignClients
 class AdminApplication
 
 fun main(args: Array<String>) {
